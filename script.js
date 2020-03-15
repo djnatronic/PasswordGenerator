0826d8bcf7367cfa29171d9502ec64dc81e94391
Nathan Bowdish <djnatronic@gmail.com>
2020-03-15 15:18:19 -0700

    initial push

---
+++ script.js
@@ -0,0 +1,20 @@
+/****
+ * WRITE YOUR CODE HERE
+ */
+
+//////////////////////////////////////////////////////////////
+// DO NOT TOUCH THE CODE BELOW
+//////////////////////////////////////////////////////////////
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
